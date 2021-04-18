4a1eecbe6ae32f1734e9cc1e7d8a3f8b7c6fe8b2
Mohsen <mabbasidesign@yahoo.com>
2021-04-18 13:44:02 +0430

    Appointment Service Get Doctors Part 1

--- AppointmentScheduler/Services/AppointmentService.cs
+++ AppointmentScheduler/Services/AppointmentService.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using AppointmentScheduler.Models;
+using AppointmentScheduler.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AppointmentScheduler.Services
 {
-    public class AppointmentService: IAppointmentService
+    public class AppointmentService : IAppointmentService
     {
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public List<DoctorVM> GetDoctorList()
+        {
+            var doctors = (from user in _db.Users
+                           select new DoctorVM
+                           {
+                               Id = user.Id,
+                               Name = user.Name
+                           }
+                           ).ToList();
+
+            return doctors;
+        }
+
+        public List<PatientVM> GetPatientList()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- AppointmentScheduler/Services/IAppointmentService.cs
+++ AppointmentScheduler/Services/IAppointmentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppointmentScheduler.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,7 @@ namespace AppointmentScheduler.Services
 {
     interface IAppointmentService
     {
+        public List<DoctorVM> GetDoctorList();
+        public List<PatientVM> GetPatientList();
     }
 }
