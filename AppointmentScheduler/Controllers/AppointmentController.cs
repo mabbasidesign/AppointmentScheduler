d761c372ee92a0df1adb461b64ff68a76d2ad278
Mohsen <mabbasidesign@yahoo.com>
2021-04-19 15:05:49 +0430

    Modal for Add_Edit Appointment

--- AppointmentScheduler/Controllers/AppointmentController.cs
+++ AppointmentScheduler/Controllers/AppointmentController.cs
@@ -10,14 +10,18 @@ namespace AppointmentScheduler.Controllers
     public class AppointmentController : Controller
     {
         private readonly IAppointmentService _appointmentService;
+
         public AppointmentController(IAppointmentService appointmentService)
         {
             _appointmentService = appointmentService;
         }
 
-         public IActionResult Index()
+        public IActionResult Index()
         {
+
             ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
+
             return View();
         }
     }
