2f02fde5f8ed40e4b330fe57341f7592111cfb24
Mohsen <mabbasidesign@yahoo.com>
2021-04-19 22:57:29 +0430

    Get Appointment Details API

--- AppointmentScheduler/Services/IAppointmentService.cs
+++ AppointmentScheduler/Services/IAppointmentService.cs
@@ -13,6 +13,9 @@ namespace AppointmentScheduler.Services
         public Task<int> AddUpdate(AppointmentVM model);
 
         public List<AppointmentVM> DoctorsEventsById(string doctorId);
+
         public List<AppointmentVM> PatientsEventsById(string patientId);
+
+        public AppointmentVM GetById(int id);
     }
 }
