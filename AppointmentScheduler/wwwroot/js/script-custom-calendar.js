460510f3cb3d210acbd95e7441c3d9bcba1471a5
Mohsen <mabbasidesign@yahoo.com>
2021-04-20 09:59:12 +0430

    Confirm and Delete Appointment Demo

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -41,7 +41,7 @@ function InitializeCalendar() {
                                         description: data.description,
                                         start: data.startDate,
                                         end: data.endDate,
-                                        backgroundColor: data.isDoctorApprove ? "#28a745" : "#dc3545",
+                                        backgroundColor: data.isDoctorApproved ? "#28a745" : "#dc3545",
                                         borderColor: "#162466",
                                         textColor: "white",
                                         id: data.id
@@ -184,4 +184,52 @@ function getEventDetailsByEventId(info) {
 
 function onDoctorChange() {
     calendar.refetchEvents();
+}
+
+function onDeleteAppointment() {
+    var id = parseInt($("#id").val());
+    $.ajax({
+        url: routeURL + '/api/Appointment/DeleteAppoinment/' + id,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+
+            if (response.status === 1) {
+                $.notify(response.message, "success");
+                calendar.refetchEvents();
+                onCloseModal();
+            }
+            else {
+
+                $.notify(response.message, "error");
+            }
+        },
+        error: function (xhr) {
+            $.notify("Error", "error");
+        }
+    });
+}
+
+function onConfirm() {
+    var id = parseInt($("#id").val());
+    $.ajax({
+        url: routeURL + '/api/Appointment/ConfirmEvent/' + id,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+
+            if (response.status === 1) {
+                $.notify(response.message, "success");
+                calendar.refetchEvents();
+                onCloseModal();
+            }
+            else {
+
+                $.notify(response.message, "error");
+            }
+        },
+        error: function (xhr) {
+            $.notify("Error", "error");
+        }
+    });
 }
